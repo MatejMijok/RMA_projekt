8a0087be5436099ea232c0a45646c0b23c92c134
Matej <matej.mijok@gmail.com>
2024-05-08 12:39:02 +0200

    Log in page design

--- app/src/main/java/hr/ferit/rmaprojekt/MainActivity.kt
+++ app/src/main/java/hr/ferit/rmaprojekt/MainActivity.kt
@@ -2,18 +2,43 @@ package hr.ferit.rmaprojekt
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import hr.ferit.rmaprojekt.ui.theme.RMAProjektTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             RMAProjektTheme {
@@ -31,10 +56,53 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var username by remember { mutableStateOf(TextFieldValue("")) }
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFDDE1F9)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ){
+        Text(
+            text = "Log in",
+            fontSize = 30.sp,
+            modifier = modifier.padding(bottom = 12.dp),
+            color = Color(0xFF161B2C)
+        )
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            placeholder = {Text(text = "Username")},
+            shape = RoundedCornerShape(15.dp),
+            modifier = modifier.padding(bottom = 8.dp)
+
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            placeholder = {Text(text = "Password")},
+            visualTransformation = PasswordVisualTransformation(),
+            shape = RoundedCornerShape(15.dp),
+            modifier = modifier.padding(bottom = 8.dp)
+        )
+        Button(
+            onClick = { /* TODO */ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF4B5C92),
+                contentColor = Color(0xFFDDE1F9)
+            ),
+            modifier = modifier
+                .width(192.dp)
+                .height(36.dp)
+        ) {
+            Text("Log In")
+
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
